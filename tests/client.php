295f6bc4db899a86b4186bad9b99932412b1fb02
Konstantinos Filios <kf@out-there-media.com>
2013-06-07 16:22:25 +0300

    Multi-call does work

--- tests/client.php
+++ tests/client.php
@@ -12,12 +12,56 @@ spl_autoload_register(function ($class) {
     include '../components/'.$class. '.php';
 });
 
-$responseObject = CBHttpMessageRequest::create('GET', $serverUrl)
-		->setGetParams(array(
-			'chunkDelays' => array(1, 2)
-		))
-		->createCall()
-		->setTimeoutSeconds(20)
-		->exec()->validateStatus()->getRawBody();
+$chunkDelaySets = array(
+	array(1, 2),
+	array(3, 1),
+	array(2, 4),
+	array(1, 1),
+	array(5, 0),
+	array(2, 2, 2),
+	array(1, 3, 1),
+	array(1, 1, 1, 1, 1)
+);
 
-print($responseObject);
+//
+// Assemble array of calls
+//
+$calls = array();
+$serialDelayTime = 0;
+$parallelDelayTime = 0;
+foreach ($chunkDelaySets as $chunkDelays) {
+	// Calculate delays
+	$callDelayTime = array_sum($chunkDelays);
+	$serialDelayTime += $callDelayTime;
+	$parallelDelayTime = max($callDelayTime, $parallelDelayTime);
+
+	// Create the call. Set 1 second buffer for timeout
+	$calls[] = CBHttpMessageRequest::create('GET', $serverUrl)
+		->setGetParam('chunkDelays', $chunkDelays)
+		->createCall()
+		->setTimeoutSeconds($callDelayTime + 1);
+}
+
+//
+// Warn the user before executing
+//
+echo "Prepared ".count($calls)." calls with expected delay times:\n";
+echo " - Serial execution: ".$serialDelayTime." sec\n";
+echo " - Parallel execution: ".$parallelDelayTime." sec\n\n";
+
+//
+// Perform the multi-call
+//
+$multiCall = new CBHttpMultiCallCurlParallel($calls);
+
+foreach ($multiCall->exec()->getResponseMessages() as $key=>$responseMessage) {
+	/* @var $responseMessage CBHttpMessageResponse */
+	$responseObject = $responseMessage->validateStatus()->getRawBody();
+
+	print($responseObject."\n");
+}
+
+//
+// Dump results
+//
+print("All calls executed in ".$multiCall->getExecutionSeconds()." sec\n");
