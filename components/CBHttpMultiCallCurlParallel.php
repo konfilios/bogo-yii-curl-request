c410d0f3715e1b11d3f1bd3057ad018942823af1
Konstantinos Filios <kf@out-there-media.com>
2013-06-18 12:20:11 +0300

    Fixing windows bug

--- components/CBHttpMultiCallCurlParallel.php
+++ components/CBHttpMultiCallCurlParallel.php
@@ -76,10 +76,40 @@ class CBHttpMultiCallCurlParallel extends CBHttpMultiCall
 			$numberReady = curl_multi_select($multiHandle);
 
 			if ($numberReady != -1) {
-				// Pull in any new data, or at least handle timeouts
-				do {
-					$execReturnValue = curl_multi_exec($multiHandle, $runningHandles);
-				} while ($execReturnValue == CURLM_CALL_MULTI_PERFORM);
+				// Patch for windows
+				// https://bugs.php.net/bug.php?id=63411 [2012-11-15 11:42 UTC] bfanger@gmail.com
+				usleep(100000);
+			}
+
+			// Pull in any new data, or at least handle timeouts
+			do {
+				$execReturnValue = curl_multi_exec($multiHandle, $runningHandles);
+			} while ($execReturnValue == CURLM_CALL_MULTI_PERFORM);
+
+			// http://www.onlineaspect.com/2009/01/26/how-to-use-curl_multi-without-blocking/
+			if ($execReturnValue == CURLM_OK) {
+				// a request was just completed -- find out which one
+				while ($done = curl_multi_info_read($multiHandle)) {
+					$info = curl_getinfo($done['handle']);
+
+//					if ($info['http_code'] == 200)  {
+//						$output = curl_multi_getcontent($done['handle']);
+//
+//						// request successful.  process output using the callback function.
+//						$callback($output);
+//
+//						// start a new request (it's important to do this before removing the old one)
+//						$ch = curl_init();
+//						$options[CURLOPT_URL] = $urls[$i++];  // increment i
+//						curl_setopt_array($ch,$options);
+//						curl_multi_add_handle($master, $ch);
+//
+//						// remove the curl handle that just completed
+//						curl_multi_remove_handle($master, $done['handle']);
+//					} else {
+//						// request failed.  add error handling.
+//					}
+				}
 			}
 		}
 		$this->stopTimer();
